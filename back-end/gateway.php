b5826f9713233589f5f8ae97976c4816859ff045
farbubby <farhan.mahbub@ucf.edu>
2023-09-21 16:37:44 -0400

    implemented create/update contact and create user

--- back-end/gateway.php
+++ back-end/gateway.php
@@ -5,22 +5,40 @@ class Gateway {
 
     private $db = null;
 
-    public function __construct() {
-        $database = new DataBase();
-        $this->db = $database->getDataBase();
+    public function __construct($db) {
+        $this->db = $db->getDataBase();
     }
 
     public function createUser(Array $data) {
 
-        $sql = "INSERT INTO user (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO user (username, password) VALUES (:username, :password)";
         $stmt = $this->db->prepare($sql);
-        $stmt->execute([$data['username'], $data['password']]);
+        $stmt->execute(['username' => $data['username'], 'password' => $data['password']]);
 
         return $this->db->lastInsertId();
     }
+
+    public function createContact(Array $data) {
+
+        $sql = "INSERT INTO contact (id, firstname, lastname, email, phone, record) VALUES (:id, :firstname, :lastname, :email, :phone, :record)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['id' => $data['id'], 'firstname' => $data['firstname'], 'lastname' => $data['lastname'], 'email' => $data['email'], 'phone' => $data['phone'], 'record' => $data['record']])
+
+        return $this->db->lastInsertId();
+    }
+
+    publlic function updateContact(Array $data, $contact_id) {
+            
+            $sql = "UPDATE contact SET firstname = :firstname, lastname = :lastname, email = :email, phone = :phone, record = :record WHERE contact_id = :contact_id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute(['firstname' => $data['firstname'], 'lastname' => $data['lastname'], 'email' => $data['email'], 'phone' => $data['phone']]);
+    
+            return $stmt->rowCount();
+    }
+
 }
 
-$gate = new Gateway();
+$gate = new Gateway(new DataBase());
 $gate->createUser([
-    'username' => 'John',
-    'password' => 'Doe']);
+    'username' => 'John1',
+    'password' => 'Doe1']);
