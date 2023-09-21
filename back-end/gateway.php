84401506736236300defd1996fb016dc3bb53026
farbubby <farhan.mahbub@ucf.edu>
2023-09-21 17:01:35 -0400

    implemented the rest of the CRUD for contacts

--- back-end/gateway.php
+++ back-end/gateway.php
@@ -11,7 +11,7 @@ class Gateway {
 
     public function createUser(Array $data) {
 
-        $sql = "INSERT INTO user (username, password) VALUES (:username, :password)";
+        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
         $stmt = $this->db->prepare($sql);
         $stmt->execute(['username' => $data['username'], 'password' => $data['password']]);
 
@@ -20,7 +20,7 @@ class Gateway {
 
     public function createContact(Array $data) {
 
-        $sql = "INSERT INTO contact (id, firstname, lastname, email, phone, record) VALUES (:id, :firstname, :lastname, :email, :phone, :record)";
+        $sql = "INSERT INTO contacts (id, firstname, lastname, email, phone, record) VALUES (:id, :firstname, :lastname, :email, :phone, :record)";
         $stmt = $this->db->prepare($sql);
         $stmt->execute(['id' => $data['id'], 'firstname' => $data['firstname'], 'lastname' => $data['lastname'], 'email' => $data['email'], 'phone' => $data['phone'], 'record' => $data['record']])
 
@@ -29,11 +29,29 @@ class Gateway {
 
     publlic function updateContact(Array $data, $contact_id) {
             
-            $sql = "UPDATE contact SET firstname = :firstname, lastname = :lastname, email = :email, phone = :phone, record = :record WHERE contact_id = :contact_id";
-            $stmt = $this->db->prepare($sql);
-            $stmt->execute(['firstname' => $data['firstname'], 'lastname' => $data['lastname'], 'email' => $data['email'], 'phone' => $data['phone']]);
+        $sql = "UPDATE contacts SET firstname = :firstname, lastname = :lastname, email = :email, phone = :phone, WHERE contact_id = :contact_id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['firstname' => $data['firstname'], 'lastname' => $data['lastname'], 'email' => $data['email'], 'phone' => $data['phone']]);
     
-            return $stmt->rowCount();
+        return $stmt->rowCount();
+    }
+
+    public function deleteContact($contact_id) {
+
+        $sql = "DELETE FROM contacts WHERE contact_id = :contact_id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['contact_id' => $contact_id]);
+
+        return $stmt->rowCount();
+    }
+
+    public function getContacts($id) {
+            
+        $sql = "SELECT * FROM contacts WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['id' => $id]);
+    
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
 }
